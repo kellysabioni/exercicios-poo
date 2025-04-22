da43f03196fcb9c124b522573321c9ad51adc0ef
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-04-22 09:41:42 -0300

    Atualiza de acordo com o novo diagrama

---
+++ src/Models/Livro.php
@@ -0,0 +1,27 @@
+<?php
+
+class Livro {
+    private string $titulo;
+    private string $autor;
+    
+    public function __construct(string $titulo,string $autor,int $paginas)
+    {
+        $this->setTitulo($titulo);
+        $this->setAutor($autor);
+    }
+
+    private function setTitulo (string $titulo):void{
+        $this->titulo = $titulo;
+    }
+    private function setAutor (string $autor):void{
+        $this->autor = $autor;
+    }
+
+    public function getTitulo () {
+        return $this->titulo;
+    }
+    public function getAutor () {
+        return $this->autor;
+    }
+
+}
