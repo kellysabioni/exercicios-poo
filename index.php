d7ac2e8cb0fa747b02e46b27d00d5d713cff1388
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-04-22 11:03:28 -0300

    atualiza

--- index.php
+++ index.php
@@ -1,33 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 03</title>
 </head>
+
 <body>
     <h1>Exercício 03</h1>
     <hr>
-<?php
-require_once 'src/Livro.php';
+    <?php
+    require_once 'src/Enums/Genero.php';
+    require_once 'src/Enums/Formato.php';
+    require_once 'src/Models/Livro.php';
+    require_once 'src/Models/Literario.php';
 
-$livro1 = new Livro("A Guerra dos Tronos", "George R. R. Martin", 694);
-$livro2 = new Livro("A Seleção", "Kiera Cass", 464);
+    $livro1 = new Literario("A Guerra dos Tronos", "George R. R. Martin", Genero::AVENTURA);
+    $livro2 = new Literario("A Seleção", "Kiera Cass", Genero::AVENTURA);
 
-?>
+    ?>
 
     <p><i>Faça as chamadas e testes nesta página</i></p>
 
     <ul>
         <li>Titulo: <?= $livro1->getTitulo(); ?></li>
         <li>Autor: <?= $livro1->getAutor(); ?></li>
-        <li>Paginas: <?= $livro1->getPaginas(); ?></li>
+        <li>Genero: <?= $livro1->getGenero()->name; ?></li>
+
     </ul>
     <ul>
         <li>Titulo: <?= $livro2->getTitulo(); ?></li>
         <li>Autor: <?= $livro2->getAutor(); ?></li>
-        <li>Paginas: <?= $livro2->getPaginas(); ?></li>
+        <li>Genero: <?= $livro2->getGenero()->name; ?></li>
+
     </ul>
-    
+
 </body>
+
 </html>
